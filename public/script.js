ba3041592ee7865ecb6b78d28676aacac006b79e
Michael DeMarco <michaelfromyeg@gmail.com>
2020-05-06 20:54:31 -0600

    Client-side host/viewer working, server side to come!

--- public/script.js
+++ public/script.js
@@ -1,11 +1,33 @@
 const socket = io("http://localhost:3000");
 
+//TODO: Separate into lobby code and room code, if it gets too long
+
+// For room
 const sendContainer = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
+const roleLabel = document.getElementById("role");
 
-const name = prompt("What is your name?");
-appendMessage("You joined");
-socket.emit("new-user", name);
+const buttonHost = document.getElementById("add-host");
+const buttonViewer = document.getElementById("add-viewer");
+
+// For lobby
+const roomContainer = document.getElementById("room-container");
+
+if (sendContainer != null) {
+  // const name = prompt("What is your name?");
+  const name = "Testing";
+  appendMessage("You joined");
+  socket.emit("new-user", roomName, name);
+
+  sendContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`);
+    socket.emit("send-chat-message", roomName, message);
+    messageInput.value = "";
+  });
+}
 
 socket.on("chat-message", (data) => {
   console.log("data", data);
@@ -19,20 +41,28 @@ socket.on("user-connected", (name) => {
 
 socket.on("user-disconnected", (name) => {
   console.log("name", name);
-  appendMessage(`${name} discconnected`);
+  appendMessage(`${name} disconnected`);
 });
 
 socket.on("logger", (data) => {
   console.log(data);
 });
 
-sendContainer.addEventListener("submit", (e) => {
-  e.preventDefault();
+socket.on("room-created", (room) => {
+  /*
+  <div>
+      <%= room %>&nbsp;&nbsp;<a href="/<%= room %>">Join</a>
+  </div>
+  */
+  const roomElement = document.createElement("div");
+  roomElement.innerText = `${room} `;
 
-  const message = messageInput.value;
-  appendMessage(`You: ${message}`)
-  socket.emit("send-chat-message", message);
-  messageInput.value = "";
+  const linkElement = document.createElement("a");
+  linkElement.href = `/${room}`;
+  linkElement.innerText = `Join`;
+
+  roomElement.append(linkElement);
+  roomContainer.append(roomElement);
 });
 
 function appendMessage(message) {
@@ -42,4 +72,27 @@ function appendMessage(message) {
 
   messageElement.append(tableData);
   sendContainer.append(messageElement);
+}
+
+function setRole(role) {
+  if (!(role === "Host" || role === "Viewer")) {
+    throw new Error("Invalid role provided to setRole() function");
+  }
+
+  roleLabel.innerText = role;
+
+  let context = {
+    role: role,
+    name: name,
+    id: socket.id,
+  };
+
+  removeButtons();
+
+  socket.emit("set-role", roomName, context);
+}
+
+function removeButtons() {
+  buttonHost.parentNode.removeChild(buttonHost);
+  buttonViewer.parentNode.removeChild(buttonViewer);
 }
--- server.js
+++ server.js
@@ -7,47 +7,95 @@ const io = require("socket.io")(server);
 // Setup the app
 app.set("views", "./views");
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
-// Initialize users, rooms as empty
-const users = {};
+// Initialize rooms as empty
+// Each room consits of some users, some hosts, and some viewers
+// Users can't see any messages until they choose a role, hosts see all messages, and viewers only see messages between themselves and the host
 const rooms = {
-  test: {},
-  clubPenguin: {}
+  lecture1: { 
+    users: {},
+    hosts: {},
+    viewers: {},
+  },
+  lecture2: { 
+    users: {},
+    hosts: {},
+    viewers: {},
+  },
 };
 
-server.listen(3000)
+server.listen(3000);
 
 app.get("/", (req, res) => {
   res.render("index", { rooms: rooms });
 });
 
 app.get("/:room", (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect("/");
+  }
   res.render("room", { roomName: req.params.room });
+});
+
+app.post("/room", (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect("/");
+  }
+  rooms[req.body.room] = { 
+    users: {},
+    hosts: {},
+    viewers: {}
+  };
+  res.redirect(req.body.room);
+  // Send message that new room was created
+  io.emit("room-created", req.body.room);
 });
 
 io.on("connection", (socket) => {
   socket.emit("logger", "Hello, world!");
 
-  socket.on("send-chat-message", (message) => {
+  socket.on("send-chat-message", (room, message) => {
     console.log(message);
 
     const context = {
       message: message,
-      name: users[socket.id],
+      name: rooms[room].users[socket.id],
     };
 
-    socket.broadcast.emit("chat-message", context);
+    socket.to(room).broadcast.emit("chat-message", context);
   });
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", users[socket.id]);
-    delete users[socket.id];
+    getUserRooms(socket).forEach((room) => {
+      socket
+        .to(room)
+        .broadcast.emit("user-disconnected", rooms[room].users[socket.id]);
+      delete rooms[room].users[socket.id];
+    });
   });
 
-  socket.on("new-user", (name) => {
-    users[socket.id] = name;
-    socket.broadcast.emit("user-connected", name);
+  socket.on("new-user", (room, name) => {
+    socket.join(room);
+    rooms[room].users[socket.id] = name;
+    socket.to(room).broadcast.emit("user-connected", name);
+  });
+
+  socket.on("set-role", (room, context) => {
+    if (context.role === "Viewer") {
+      console.log("Add a viewer");
+    } else if (context.role === "Host") {
+      console.log("Add a host");
+    } else {
+      throw new Error("Invalid role provided to set-role socket listener");
+    }
   });
 });
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name);
+    return names;
+  }, []);
+}
